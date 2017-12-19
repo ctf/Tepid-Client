3b94409c005d1340c8e8a5a893544c96b925318a
Seiji Amasawa <samasawa95@gmail.com>
2017-12-18 20:46:49 -0500

    some small changes to Win32Icon.java

--- src/main/java/dorkbox/systemTray/windows/Win32Icon.java
+++ src/main/java/dorkbox/systemTray/windows/Win32Icon.java
@@ -10,45 +10,53 @@ import java.nio.ByteOrder;
 public class Win32Icon {
 
     public static final int BI_RGB = 0, BI_RLE8 = 1, BI_RLE4 = 2, BI_BITFIELDS = 3, BI_JPEG = 4, BI_PNG = 5;
-
+    
     public static byte[] imageToIco(BufferedImage img, int... sizes) throws IOException {
+    	BufferedImage[] resized = new BufferedImage[sizes.length];
+    	for (int i = 0; i < sizes.length; i++) {
+			resized[i] = scale(img, sizes[i]);
+		}
+    	return imageToIco(resized);
+    }
+
+    public static byte[] imageToIco(BufferedImage... sizes) throws IOException {
         int size = 0;
-        for (int s : sizes) {
-            size += bmpSize(s);
+        for (BufferedImage s : sizes) {
+            size += bmpSize(s.getWidth(), s.getHeight());
         }
         ByteBuffer ico = ByteBuffer.allocate(6 + (16 * sizes.length) + size - (14 * sizes.length)).order(ByteOrder.LITTLE_ENDIAN);
         ico.putShort((short) 0);
         ico.putShort((short) 1); //image type (1==icon)
         ico.putShort((short) sizes.length); //number of subimages
         int pos = ico.position() + 16 * sizes.length;
-        for (int s : sizes) {
+        for (BufferedImage s : sizes) {
             //write icondirentry
-            ico.put((byte) (0xff & s));
-            ico.put((byte) (0xff & s));
+            ico.put((byte) (0xff & s.getWidth()));
+            ico.put((byte) (0xff & s.getHeight()));
             ico.putShort((short) 0);
             ico.putShort((short) 1); //number of color planes
             ico.putShort((short) 32); //32-bit
-            ico.putInt(bmpSize(s) - 14);
+            ico.putInt(bmpSize(s.getWidth(), s.getHeight()) - 14);
             ico.putInt(pos);
-            pos += bmpSize(s) - 14;
+            pos += bmpSize(s.getWidth(), s.getHeight()) - 14;
         }
-        for (int s : sizes) {
-            byte[] bmp = imageToBitmap(scale(img, s));
+        for (BufferedImage s : sizes) {
+            byte[] bmp = imageToBitmap(s);
             ico.put(bmp, 14, bmp.length - 14);
         }
         return ico.array();
     }
 
-    public static int bmpSize(int s) {
-        final int rowSize = (int) (Math.floor((32 * s + 31) / 32) * 4),
-                pixelArraySize = rowSize * s;
+    public static int bmpSize(int w, int h) {
+        final int rowSize = (int) (Math.floor((32 * w + 31) / 32) * 4),
+                pixelArraySize = rowSize * h;
         return 14 + 40 + pixelArraySize + pixelArraySize / 16;
     }
 
     public static BufferedImage scale(BufferedImage img, int s) {
         BufferedImage out = new BufferedImage(s, s, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = out.createGraphics();
-//        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
         g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g.drawImage(img, 0, 0, s, s, null);
         g.dispose();

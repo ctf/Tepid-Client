9ef7a6b0cc8c97eaa14f73ace20fddbdee7188d6
Seiji Amasawa <samasawa95@gmail.com>
2017-12-19 22:16:26 -0500

    remove local version of Q and add github version as dependency

--- src/main/java/in/waffl/q/PromiseCallback.java
+++
@@ -1,8 +0,0 @@
-package in.waffl.q;
-
-public abstract class PromiseCallback<T> {
-    public abstract void resolved(T result);
-
-    public void rejected(String reason, Throwable cause) {
-    }
-}

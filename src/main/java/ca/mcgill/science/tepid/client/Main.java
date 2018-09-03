5b7b239e8acff9a372f9d9bc838fce4f388cf9d1
Daniel Goldman <merkavabuilder@gmail.com>
2018-09-03 16:11:30 -0400

    add hacky fix for removal of the --clean option

--- src/main/java/ca/mcgill/science/tepid/client/Main.java
+++ src/main/java/ca/mcgill/science/tepid/client/Main.java
@@ -6,6 +6,12 @@ import com.bugsnag.Bugsnag;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Hacky fix for the removal of the --clean option
+        if (args.length > 0) {
+            System.exit(0);
+        }
+
         Bugsnag bugsnager = new Bugsnag("5870fc6e49bb500adc05defc7703a25e");
         Client client = Client.create(MainObserver.INSTANCE);
     }

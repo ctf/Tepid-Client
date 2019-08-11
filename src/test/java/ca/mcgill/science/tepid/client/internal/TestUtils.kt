66988dd0a0af610333ae2991be2701c4a8d7af53
Daniel Goldman <merkavabuilder@gmail.com>
2019-08-10 22:24:47 -0400

    upgrade models and TestUtilsDelegate

--- src/test/java/ca/mcgill/science/tepid/client/internal/TestUtils.kt
+++ src/test/java/ca/mcgill/science/tepid/client/internal/TestUtils.kt
@@ -1,8 +1,15 @@
 package ca.mcgill.science.tepid.client.internal
 
 import ca.mcgill.science.tepid.test.TestUtilsDelegate
+import ca.mcgill.science.tepid.utils.PropsLDAPTestUser
+import ca.mcgill.science.tepid.utils.PropsURL
 import java.io.File
 
-object TestUtils : TestUtilsDelegate() {
+object TestUtils : TestUtilsDelegate(
+        PropsLDAPTestUser.TEST_USER,
+        PropsLDAPTestUser.TEST_PASSWORD,
+        PropsURL.SERVER_URL_PRODUCTION!!,
+        PropsURL.TESTING!!.toBoolean()
+) {
     val testFile = "pdf-test.pdf"
 }
